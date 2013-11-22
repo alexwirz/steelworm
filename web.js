5e70f5728da22a3a302ee63796d1a40320119b18
Alexander Wirz <alexanderwirz@alexanders-mbp.fritz.box>
2013-11-22 23:08:18 +0100

    show simple form

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
+var fs = require("fs")
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -10,6 +11,18 @@ app.get('/', function(request, response) {
 app.post('/', function(request, response) {
 	var name = request.body.name;
 	response.send('Hello ' + name + '!');
+});
+
+
+app.get('/hello', function(request, response) {
+	fs.readFile('./hello.html', function (err, html) {
+    if (err) {
+			throw err; 
+    }
+		response.setHeader ("Content-Type", "text/html");
+  	response.send(html);
+		response.end();
+	});
 });
 
 var port = process.env.PORT || 5000;
