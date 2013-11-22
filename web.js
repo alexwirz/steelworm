8dd51236931fa6f734b0189542b0b842e5f59d3b
Alexander Wirz <alexanderwirz@alexanders-mbp.fritz.box>
2013-11-22 22:45:28 +0100

    post

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.post('/', function(request, response) {
+	var name = request.body.name;
+	response.send('Hello ' + name + '!');
 });
 
 var port = process.env.PORT || 5000;
