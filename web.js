c4ed72c931f5c8ff4fb17e86424358c5c390d5d8
Alexander Wirz <alexanderwirz@alexanders-mbp.lan>
2014-02-23 18:12:37 +0100

    added ip controller to show users location

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 var fs = require("fs")
+var geoip = require('geoip');
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -13,14 +14,26 @@ app.post('/', function(request, response) {
 	response.send('Hello ' + name + '!');
 });
 
+app.get('/ip', function(request, response) {
+	var ip = request.connection.remoteAddress;
+	var city = new geoip.City('/Users/alexanderwirz/Downloads/GeoLiteCity.dat');
+	var clientCity = city.lookupSync (ip);
+	console.log(clientCity);
+	if (clientCity == null) {
+		response.send ('Could not figure out your location :( Your ip is ' + ip);
+	} else {
+        	response.send('Your ip is ' + ip + '. You are in ' + clientCity.country_name + '/' + clientCity.city);
+	}
+    }
+);
 
 app.get('/hello', function(request, response) {
 	fs.readFile('./hello.html', function (err, html) {
-    if (err) {
+		if (err) {
 			throw err; 
-    }
+		}
 		response.setHeader ("Content-Type", "text/html");
-  	response.send(html);
+		response.send(html);
 		response.end();
 	});
 });
