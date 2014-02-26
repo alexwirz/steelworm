22033ae462fce6dd0385f050811d99bd8b52642e
Alexander Wirz <alex.wirz@gmail.com>
2014-02-26 18:17:19 +0100

    added a test controller for oauth with github

--- web.js
+++ web.js
@@ -46,6 +46,46 @@ app.get('/hello', function(request, response) {
 	});
 });
 
+app.get('/login', function(request, response) {
+	// 90-er! ;)
+	fs.readFile('./login-with-github.html', function (err, html) {
+		if (err) {
+			throw err;
+		}
+
+		response.setHeader ("Content-Type", "text/html");
+		response.send (html);
+		response.end ();
+	});
+});
+
+app.get ('/gh-oauth', function (request, response){
+	var idpGithub = require('idp-github')(
+		{ clientID: '139369522cd1d37bb51f',
+		clientSecret: '4311d291fda987b489b3b992d3a8727030383c89',
+		redirectUri: 'http://steelworm.herokuapp.com/gh-login' });
+
+	// Get the auth URL to redirect the user for OAuth2 login
+	var code = idpGithub.authUrl({ state: 'super important stuff' });
+	console.log ('code : ' + code);
+
+	// Using the code provided in the query string upon return to your web app, get the identity:
+	idpGithub.identity(code, function (err, identity) {
+		if (err) {
+			console.error('login with github failed!');
+			console.log ('err: ' + JSON.stringify(err));
+			throw err;
+		} else {
+			response.send ('Hello, ' + identity.name);
+		}
+	});
+});
+
+//app.get ('/gh-login', function (request, response) {
+//	response.write ('gh-login called...');
+//	response.end ();
+//});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
