4def702af256355c1510592a23e5fc5ec08e9988
Alexander Wirz <alex.wirz@gmail.com>
2014-02-27 01:10:16 +0100

    generate random session secret

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require("express");
 var passport = require ("passport");
 var GitHubStrategy = require ('passport-github').Strategy;
 var app = express();
-var fs = require("fs");
 
 passport.use(new GitHubStrategy({
     clientID: '139369522cd1d37bb51f',
@@ -25,9 +24,26 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
+function randomString(length, chars) {
+    var mask = '';
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (chars.indexOf('#') > -1) mask += '0123456789';
+    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+    var result = '';
+    for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
+    return result;
+}
+
+function sessionKey () {
+	var sessionKey = randomString (32, 'aA#!');
+	console.log ('sessionKey : ' + sessionKey);
+	return sessionKey;
+}
+
 app.configure(function() {
 	app.use (express.cookieParser());
-	app.use (express.cookieSession ({secret : 'V3ryS3cr3tSh!t!', cookie: { maxAge: 60 * 1000 }}));
+	app.use (express.cookieSession ({secret : sessionKey (), cookie: { maxAge: 60 * 1000 }}));
   	app.use (express.bodyParser());
   	app.use (express.logger ());
 	app.use (passport.initialize());
